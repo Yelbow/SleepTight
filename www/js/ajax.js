d81108fc2e60ca5b1afb5df9e9a0064df606bf29
Yelbow <pieterse96@gmail.com>
2017-04-19 13:25:31 +0200

    requests naar sensor 15

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
 
   var getClientData = function(){
-    $.get("http://u5231p3363.web0091.zxcs.nl/Sleeptight/client/3", function(result){
+    $.get("http://u5231p3363.web0091.zxcs.nl/Sleeptight/api/client/3", function(result){
       console.log(result)
     })
   }
