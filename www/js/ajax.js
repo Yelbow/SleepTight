bc1960cfc2353ebaa9709fb4083885b165e1da4a
Pablo Cleij <pablocley@hotmail.com>
2017-04-19 13:01:54 +0200

    Added ajax.js

---
+++ www/js/ajax.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function () {
+
+  var getClientData = function(){
+    $.get("http://u5231p3363.web0091.zxcs.nl/Sleeptight/client/3", function(result){
+      console.log(result)
+    })
+  }
+
+
+}
